60a3ab0894f91f17038bd5556558c684ac2b3f4f
devlulcas <lucasalvesrego.contato@gmail.com>
2021-05-21 12:31:32 -0300

    added more things about associative arrays

--- syntax/associative_arrays.php
+++ syntax/associative_arrays.php
@@ -35,5 +35,84 @@
       </li>
     </ul>
   </article>
-</body>
+
+  <p>I think that the concept is really similar to JS objects... Key/value</p>
+  <pre>
+    <?php
+      $associative_array = [
+        'key' => 'value',
+        'name' => 'Lucas',
+        'age' => 18,
+        'hobbies' => [
+          1 => 'programming', 
+          2 => 'run', 
+          3 => 'polymer clay crafts'
+        ],
+        'potato' => 'and molasses'
+      ];
+      print_r($associative_array);
+      
+      echo '<br>Valor na chave "key" => '.$associative_array['key'].'<br><br>';
+      
+      $associative_array['new_key'] = 'new value';
+      print_r($associative_array)
+    ?>
+  </pre>
+  <h2>NULL COALESCING ASSIGNMENT OPERATOR ??=</h2>
+  <pre>
+    <?php
+      //Short syntax
+      $associative_array['this_key_does_not_exist_yet']??= 'assign me';
+      $associative_array['this_key_already_exists'] = 'print me';
+      $associative_array['this_key_already_exists'] ??= 'do not print me, cause I already exist';
+      
+      //Other syntax for this
+      $associative_array['unknown_key'] = $associative_array['unknown_key'] ?? 'value';
+      $associative_array['key'] = $associative_array['key'] ?? 'this will not be assigned';  
+      print_r($associative_array);
+    ?>
+  </pre>
+  
+  <h2>Print keys</h2>
+  <pre>
+    <?php
+      print_r(array_keys($associative_array));  
+    ?>
+  </pre>
+
+  <h2>Print values</h2>
+  <pre>
+    <?php
+      print_r(array_values($associative_array));  
+    ?>
+  </pre>
+
+  <h2>Sorting by keys</h2>
+  <pre>
+    <?php
+      print_r(ksort($associative_array));  
+    ?>
+  </pre>
+  
+  <h2>Sorting by values</h2>
+  <pre>
+    <?php
+      print_r(asort($associative_array));  
+    ?>
+  </pre>
+  
+  <h2>Multidimensional arrays</h2>
+  <pre>
+    <?php
+      $m_array = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [0, 0, 0]
+      ];
+
+      print_r($m_array);
+    ?>
+  </pre>
+</body> 
 </html>
